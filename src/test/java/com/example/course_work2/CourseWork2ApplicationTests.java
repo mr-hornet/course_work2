4c629c5d27d341efafe487c27b01190e1f607b80
Alexandr <mr-hornet@mail.ru>
2023-06-19 19:18:37 +1000

    Initial commit

---
+++ src/test/java/com/example/course_work2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_work2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
